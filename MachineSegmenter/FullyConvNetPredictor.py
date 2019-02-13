c3cf6ba99c21203a21fcb5073b34dc9ce2ec918b
DarioMiroli <s1033855@sms.ed.ac.uk>
2019-02-13 17:17:15 +0000

    Added more trainhing sets for agar pads

--- MachineSegmenter/FullyConvNetPredictor.py
+++ MachineSegmenter/FullyConvNetPredictor.py
@@ -1,15 +1,32 @@
 from FullyConvNet import FullyConvNet
-
+import numpy as np
+import scipy.ndimage
 #Set up and load network using GPU
 #M1 = FullyConvNet(1024,1024,True)
 #Change this !!!!
 M1 = FullyConvNet(1024,1024,False)
 
 #Get Model
-M1.loadModel()
+M1.loadModel(prompt=False,path="./Files/Models/TempModels/MostRecent_Agar_Pads_Model.h5")
 
 #Select folder to analyse
-images, imageNames = M1.loadImagesFromFolder(title= "Select folder to analyse.")
+images, imageNames = M1.loadImagesFromFolder(prompt=False,path="/home/s1033855/Desktop/CorrectedTrainingImages/Leonardo_Uncorrected",title= "Select folder to analyse.")
+
+#Pad smaller iamges
+for i,image in enumerate(images):
+    if image.shape[0] < 1024:
+        M1.displayData([image],delay=1)
+        xpadBefore = (1024-image.shape[0])/2
+        xpadAfter = (1024-image.shape[0])/2
+        ypadBefore = (1024-image.shape[1])/2
+        ypadAfter = (1024-image.shape[1])/2
+        #images[i] = np.pad(image, ((xpadBefore,xpadAfter),(ypadBefore,ypadAfter)), mode="wrap")
+        images[i] = scipy.ndimage.zoom(image, 2, order=3)
+        M1.displayData([images[i]],delay=1)
+        print(images[i].shape)
+
+
+
 #images = [x[250:250+512,250:250+512] for x in images]
 #images = [x[0:1024,0:1024] for x in images]
 
@@ -23,9 +40,11 @@ results = []
 for i in range(len(images)):
     results.append(images[i])
     results.append(predictions[i])
-M1.displayData(results)
+M1.displayData(results,delay =5)
 
 #Save images
-M1.SaveImagesToFolder(images,imageNames,title="Select folder to save to.")
-imageNames = ["prediction"+i for i in imageNames]
-M1.SaveImagesToFolder(predictions,imageNames,title="Select folder to save to.")
+save = raw_input("Save images (y/n)?")
+if save == 'y':
+    M1.SaveImagesToFolder(images,imageNames,title="Select folder to save to.")
+    imageNames = ["prediction"+i for i in imageNames]
+    M1.SaveImagesToFolder(predictions,imageNames,title="Select folder to save to.")
